31fdda71e276e8580729039913c260f5b177e912
KarolisGaiv <kgaivenis@gmail.com>
2020-11-06 00:09:34 +0200

    SOUR-15 Create Notification icon component

---
+++ src/components/Notification_Icon/Notification_Icon.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./notification_Icon.scss";
+import { ReactComponent as NotificationBell } from "assets/notification_Bell.svg";
+
+// import { ReactComponent as SourceryLogo } from "assets/logo.svg";
+
+function DisplayIcon() {
+  return (
+    <div>
+      <NotificationBell />
+    </div>
+  );
+}
+
+export default DisplayIcon;

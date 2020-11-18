8f95695e582634d8b93b02abfa969e31744a431c
Indre Makauskaite <makauskaite.indre@gmail.com>
2020-11-18 14:55:37 +0200

    SOUR-44-Add FormContainer and temporary components LoginComponent and RegistrationComponent

---
+++ src/components/Form/FormContainer/FormContainer.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Button } from "../../Button/Button";
+import "../form.scss";
+
+const FormContainer = (props) => {
+  const {
+    legend,
+    sublegend,
+    buttonText,
+    smallText,
+    path,
+    link,
+    elements,
+    submit,
+  } = props;
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    submit();
+  }
+
+  return (
+    <React.Fragment>
+      <form className="form-container" onSubmit={handleSubmit}>
+        <h2 className="form-container__legend">{legend}</h2>
+        <p className="form-container__sublegend">{sublegend}</p>
+        <div className="form-container__form-grid">{elements}</div>
+        <footer className="form-container__footer">
+          <Button className="button button--enabled">{buttonText}</Button>
+          <small className="form-container__footer-text">
+            {smallText}
+            <a className="form-container__sign-link" href={path}>
+              {link}
+            </a>
+          </small>
+        </footer>
+      </form>
+    </React.Fragment>
+  );
+};
+
+export default FormContainer;
+
+FormContainer.propTypes = {
+  legend: PropTypes.string,
+  sublegend: PropTypes.string,
+  buttonText: PropTypes.string,
+  smallText: PropTypes.string,
+  path: PropTypes.string,
+  link: PropTypes.string,
+  elements: PropTypes.object,
+  submit: PropTypes.func,
+};
---
+++ src/components/Form/LoginForm/LoginComponent.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { Redirect } from "react-router-dom";
+import FormContainer from "../FormContainer/FormContainer";
+import InputField from "../../InputField/InputField";
+import { Validators } from "../../InputField/inputValidators";
+import "./login-form.scss";
+
+export class LoginComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      pass: "",
+      text: "",
+      message: "",
+      redirect: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.submit = this.submit.bind(this);
+  }
+
+  handleChange = (key) => (value) => {
+    this.setState({ [key]: value });
+  };
+
+  submit = () => {
+    if (
+      !Validators.email(this.state.email) &&
+      !Validators.password(this.state.pass)
+    ) {
+      this.setState({
+        redirect: true,
+      });
+    }
+  };
+
+  render() {
+    if (this.state.redirect) {
+      return <Redirect to="/register" />;
+    }
+    return (
+      <FormContainer
+        legend="Login"
+        sublegend="Welcome back! Login, please"
+        buttonText="login"
+        smallText="Don&rsquo;t have an account?"
+        path="/register"
+        link="Sign up"
+        submit={this.submit}
+        elements={
+          <React.Fragment>
+            <InputField
+              label="email"
+              value={this.state.email}
+              type="email"
+              placeholder="Valid email address"
+              validators={[
+                { check: Validators.email, message: "email is not valid" },
+              ]}
+              required={true}
+              onChange={this.handleChange("email")}
+            />
+            <InputField
+              label="password"
+              value={this.state.pass}
+              type="password"
+              placeholder="At least 8 characters"
+              validators={[
+                {
+                  check: Validators.password,
+                  message: "password is not valid",
+                },
+              ]}
+              required={true}
+              onChange={this.handleChange("pass")}
+            />
+          </React.Fragment>
+        }
+      />
+    );
+  }
+}

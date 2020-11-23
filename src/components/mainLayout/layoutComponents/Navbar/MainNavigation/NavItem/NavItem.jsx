c235152fe0d88b1ed2c820e7f10270ebdc712188
KarolisGaiv <kgaivenis@gmail.com>
2020-11-23 10:40:46 +0200

    SOUR-44 Solve merge conflict

--- src/components/mainLayout/layoutComponents/Navbar/MainNavigation/NavItem/NavItem.jsx
+++ src/components/mainLayout/layoutComponents/Navbar/MainNavigation/NavItem/NavItem.jsx
@@ -6,8 +6,11 @@ import "../mainNavigation.scss";
 
 const NavItem = (props) => {
   return (
-    <li className="navigation__item">
-      {props.background}
+    <li
+      className={`navigation__item ${
+        props.expanded ? "navigation__item--long" : "navigation__item--short"
+      }`}
+    >
       <div className="navigation__context">
         <SVGIcon name={props.iconName} className={`navigation__icon`} />
         <span className={`navigation__text`}>{props.itemText}</span>
@@ -19,7 +22,7 @@ const NavItem = (props) => {
 export default NavItem;
 
 NavItem.propTypes = {
-  background: propTypes.object,
+  expanded: propTypes.bool,
   iconName: propTypes.string,
   itemText: propTypes.string,
 };

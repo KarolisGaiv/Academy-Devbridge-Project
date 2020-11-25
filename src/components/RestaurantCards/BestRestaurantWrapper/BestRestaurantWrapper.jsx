9c3a83f567d1f014e2541c8fa9eae9cf1aca5ed7
Indre Makauskaite <makauskaite.indre@gmail.com>
2020-11-25 18:42:28 +0200

    SOUR-43-Integrate db.json into RestaurantCard and BestRestaurantWrapper components

--- src/components/RestaurantCards/BestRestaurantWrapper/BestRestaurantWrapper.jsx
+++ src/components/RestaurantCards/BestRestaurantWrapper/BestRestaurantWrapper.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { RestaurantIntroCard } from "../RestaurantIntroCard/RestaurantIntroCard";
-import { RestaurantCard } from "../RestaurantCard/RestaurantCard";
+import RestaurantCard from "../RestaurantCard/RestaurantCard";
 import "./best-restaurant-wrapper.scss";
+import database from "../../../db.json";
 
-export class BestRestaurantWrapper extends React.Component {
-  render() {
-    return (
-      <div className="best-restaurant-wrapper">
-        <RestaurantIntroCard></RestaurantIntroCard>
-        <RestaurantCard></RestaurantCard>
-        <RestaurantCard></RestaurantCard>
-      </div>
-    );
-  }
-}
+const BestRestaurantWrapper = () => {
+  const restaurant = database.restaurants.restaurantList;
+  let i = Math.floor(Math.random() * Math.floor(restaurant.length));
+  let j = Math.floor(Math.random() * Math.floor(restaurant.length));
+
+  return (
+    <div className="best-restaurant-wrapper">
+      <RestaurantIntroCard></RestaurantIntroCard>
+      <RestaurantCard image={restaurant[i].image}></RestaurantCard>
+      <RestaurantCard image={restaurant[j].image}></RestaurantCard>
+    </div>
+  );
+};
+
+export default BestRestaurantWrapper;

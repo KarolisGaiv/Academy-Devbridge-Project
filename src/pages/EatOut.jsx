11cd91115b34ec4fc4ec1dc34b10a9a8400a9d49
Laurynas Stoma <laurynas.stoma@gmail.com>
2020-11-25 21:43:05 +0200

    SOUR-9 1) Build <Breadcrumbs> 2) Updated pages with <Breadcrumbs>

--- src/pages/EatOut.jsx
+++ src/pages/EatOut.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Breadcrumbs from "../components/Breadcrumbs/Breadcrumbs";
 
 const EatOut = () => {
   return (
     <main>
+      <Breadcrumbs />
       <h1 style={{ margin: "auto", textAlign: "center" }}>EatOut</h1>
     </main>
   );

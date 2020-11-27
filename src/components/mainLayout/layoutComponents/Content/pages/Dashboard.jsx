c6bd0abd9f632591a4325c2e1a8614a88e578846
Indre Makauskaite <makauskaite.indre@gmail.com>
2020-11-27 12:07:35 +0200

    SOUR-43-Add RestaurantCardWrapper to dashboard.jsx

--- src/components/mainLayout/layoutComponents/Content/pages/Dashboard.jsx
+++ src/components/mainLayout/layoutComponents/Content/pages/Dashboard.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import BestRestaurantWrapper from "../../../../RestaurantCards/BestRestaurantWrapper/BestRestaurantWrapper";
 
 const Dashboard = () => {
   return (
     <main>
-      <h1>Dashboard</h1>
+      <h2>Reservations</h2>
+      <BestRestaurantWrapper></BestRestaurantWrapper>
+      <h2>News and Stories</h2>
     </main>
   );
 };

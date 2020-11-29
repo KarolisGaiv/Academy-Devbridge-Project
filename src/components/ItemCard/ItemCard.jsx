425abc2b67888b1c8ef3a8269ab53e42b4b28326
KarolisGaiv <kgaivenis@gmail.com>
2020-11-29 16:05:22 +0200

    SOUR-17 add additional dummy reservations array, add props to item card component

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -3,15 +3,17 @@ import "./item-card.scss";
 import PropTypes from "prop-types";
 import SVGIcon from "../SVGIcon/SVGIcon";
 
-export const ResCard = (props) => {
+export const ResCard = ({ category, totalNumber, icon, keyword }) => {
   return (
     <div className="card-wrapper">
       <div className="card-wrapper__info">
-        <span className="card-wrapper__name">{props.name}</span>
-        <span className="card-wrapper__details">2 RESERVED</span>
+        <span className="card-wrapper__name">{category}</span>
+        <span className="card-wrapper__details">
+          {totalNumber + " " + keyword}
+        </span>
       </div>
       <div className="card-wrapper__icon">
-        <SVGIcon name={props.icon} />
+        <SVGIcon name={icon} />
       </div>
     </div>
   );
@@ -20,6 +22,8 @@ export const ResCard = (props) => {
 export default ResCard;
 
 ResCard.propTypes = {
-  name: PropTypes.string,
+  category: PropTypes.string,
   icon: PropTypes.string,
+  totalNumber: PropTypes.number,
+  keyword: PropTypes.string,
 };
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -3,8 +3,11 @@ import "./dashboard.scss";
 import WeatherWidget from "components/WeatherWidget/WeatherWidget";
 import GreetingWidget from "../../components/GreetingWidget/GreetingWidget";
 import ItemCard from "../../components/ItemCard/ItemCard";
+import database from "../../db.json";
 
 const Dashboard = () => {
+  const reservationsData = database.userData.reservations;
+
   return (
     <div className="dashboard">
       <section className="dashboard__header">
@@ -14,9 +17,24 @@ const Dashboard = () => {
       <section className="dashboard__reservations-wrapper">
         <span className="dashboard__section-title">Reservations</span>
         <div className="dashboard__reservations-cards">
-          <ItemCard name="Devices" icon="Phone" />
-          <ItemCard name="Books" icon="Book" />
-          <ItemCard name="Meeting rooms" icon="Door" />
+          <ItemCard
+            category="Devices"
+            icon="Phone"
+            totalNumber={reservationsData.devices.length}
+            keyword={"reserved"}
+          />
+          <ItemCard
+            category="Books"
+            icon="Book"
+            totalNumber={reservationsData.books.length}
+            keyword={"reserved"}
+          />
+          <ItemCard
+            category="Meeting rooms"
+            icon="Door"
+            totalNumber={reservationsData.meetingRooms.length}
+            keyword={"reserved"}
+          />
         </div>
       </section>
     </div>

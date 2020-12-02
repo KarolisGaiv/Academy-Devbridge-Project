a25c88f13da7bf63e0b50ba74a73dcffa940e1bd
Laurynas Stoma <laurynas.stoma@gmail.com>
2020-12-02 18:25:04 +0200

    Make responsive dashboard page 1. 3 Media query sizes, laptop (1200px, tablet landscape 1024px, tablet 768px) 2. Hello and weather widgets changes its positioning 3. reservation container changes its positioning 4. Restaurants container changes its grid 5. Newsfeed changes its grid

--- src/components/RestaurantCards/BestRestaurantWrapper/BestRestaurantWrapper.jsx
+++ src/components/RestaurantCards/BestRestaurantWrapper/BestRestaurantWrapper.jsx
@@ -7,23 +7,27 @@ import { Ratings } from "../../Rating/maxRatings";
 const BestRestaurantWrapper = () => {
   return (
     <div className="best-restaurants">
-      <RestaurantIntroCard />
-      <RestaurantCard
-        image={Ratings.numberOneRating()[6]}
-        checkins={Ratings.numberOneRating()[0]}
-        rating={Ratings.numberOneRating()}
-        category={Ratings.numberOneRating()}
-        title={Ratings.numberOneRating()[4]}
-        hours={Ratings.numberOneRating()[5]}
-      ></RestaurantCard>
-      <RestaurantCard
-        image={Ratings.numberTwoRating()[6]}
-        checkins={Ratings.numberTwoRating()[0]}
-        rating={Ratings.numberTwoRating()}
-        category={Ratings.numberTwoRating()}
-        title={Ratings.numberTwoRating()[4]}
-        hours={Ratings.numberTwoRating()[5]}
-      ></RestaurantCard>
+      <div className="restaurant-intro-card__wrapper">
+        <RestaurantIntroCard />
+      </div>
+      <div className="restaurant-card__wrapper">
+        <RestaurantCard
+          image={Ratings.numberOneRating()[6]}
+          checkins={Ratings.numberOneRating()[0]}
+          rating={Ratings.numberOneRating()}
+          category={Ratings.numberOneRating()}
+          title={Ratings.numberOneRating()[4]}
+          hours={Ratings.numberOneRating()[5]}
+        ></RestaurantCard>
+        <RestaurantCard
+          image={Ratings.numberTwoRating()[6]}
+          checkins={Ratings.numberTwoRating()[0]}
+          rating={Ratings.numberTwoRating()}
+          category={Ratings.numberTwoRating()}
+          title={Ratings.numberTwoRating()[4]}
+          hours={Ratings.numberTwoRating()[5]}
+        ></RestaurantCard>
+      </div>
     </div>
   );
 };

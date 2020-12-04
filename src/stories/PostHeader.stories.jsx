dd400100b761d016355152a4f50703877327c74a
Laurynas Stoma <laurynas.stoma@gmail.com>
2020-12-04 15:48:44 +0200

    Moved functional component to it's own file

--- src/stories/PostHeader.stories.jsx
+++ src/stories/PostHeader.stories.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import UserAvatar from "../components/UserAvatar/UserAvatar";
+import TimeBetween from "../components/NewsFeedCard/TimeBetween";
+//data will come through components
 import fakeData from "../db.json";
 import "../components/NewsFeedCard/PostHeader/post-header.scss";
 
@@ -10,24 +12,6 @@ export default {
 export const PostHeader = () => {
   const timeNow = new Date().getTime();
 
-  const timeBetween = (now, post) => {
-    const oneDay = 1000 * 60 * 60 * 24;
-    const oneHour = 1000 * 60 * 60;
-    const oneMinute = 1000 * 60;
-    const postTime = new Date(post).getTime();
-    const difference = now - postTime;
-    const daysBetween = Math.round(difference / oneDay);
-    const hoursBetween = Math.round(difference / oneHour);
-    const minutesBetween = Math.round(difference / oneMinute);
-    if (daysBetween > 1) {
-      return daysBetween + "d";
-    } else if (hoursBetween > 1) {
-      return hoursBetween + "h";
-    } else if (minutesBetween > 1) {
-      return minutesBetween + "min";
-    }
-  };
-
   return (
     <div className="post__header">
       {/* /index picked only for story */}
@@ -37,7 +21,7 @@ export const PostHeader = () => {
       <span className="post__username">{fakeData.stories[1].userName}</span>
       <span className="post__location">{fakeData.stories[1].postLocation}</span>
       <span className="post__date">
-        {timeBetween(timeNow, fakeData.stories[1].postDate)}
+        {TimeBetween(timeNow, fakeData.stories[1].postDate)}
       </span>
     </div>
   );

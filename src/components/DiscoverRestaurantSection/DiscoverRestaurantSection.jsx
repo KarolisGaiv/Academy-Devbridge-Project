0a007ff309a7153d8af98d794da0ddb8faa56958
Indre Makauskaite <makauskaite.indre@gmail.com>
2020-12-04 20:01:52 +0200

    SOUR-65 Create DiscoverRestaurantSection component

---
+++ src/components/DiscoverRestaurantSection/DiscoverRestaurantSection.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { RestaurantBigCard } from "../RestaurantCards/RestaurantBigCard/RestaurantBigCard";
+import "./discover-restaurant-section.scss";
+// import database from "../../../src/db.json";
+
+class DiscoverRestaurantSection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      restaurantList: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3008/restaurants")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            restaurantList: result.restaurantList,
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        }
+      );
+  }
+
+  // const restaurants = database.restaurants.restaurantList;
+  // const i = Math.floor(Math.random() * Math.floor(restaurants.length));
+  // const j = Math.floor(Math.random() * Math.floor(restaurants.length));
+  // const k = Math.floor(Math.random() * Math.floor(restaurants.length));
+
+  render() {
+    const { error, isLoaded, restaurantList } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Is loading...</div>;
+    } else {
+      return (
+        <div className="discover-restaurant-section">
+          {restaurantList.map((restaurant) => (
+            <RestaurantBigCard
+              key={restaurant.id}
+              checkins={restaurant.checkIns}
+              image={restaurant.image}
+              title={restaurant.name}
+              description={restaurant.description}
+              web={restaurant.website}
+              address={restaurant.address}
+              hours={
+                restaurant.openingHours[0].hours.substring(0, 2) +
+                ":00 " +
+                restaurant.openingHours[0].hours.substring(3, 7) +
+                ":00"
+              }
+              category={restaurant.categories}
+            />
+          ))}
+        </div>
+      );
+    }
+  }
+}
+
+export default DiscoverRestaurantSection;

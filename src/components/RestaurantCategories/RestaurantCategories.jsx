fe95930bdcadffb73530d1b28f470f30bc8a08b2
KarolisGaiv <kgaivenis@gmail.com>
2020-12-07 22:06:43 +0200

    SOUR-23 Move Restaurant categories section to seperate component folder, fix laptop breakpoint - reduce categories columns from 4 to 3

---
+++ src/components/RestaurantCategories/RestaurantCategories.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import CategoryCard from "../CategoryCard/CategoryCard";
+import data from "../../db.json";
+import "./restaurant-categories.scss";
+
+const RestaurantCategories = () => {
+  // loop through all restaurants and push their categories into one array
+  const existingCategories = data.restaurants.restaurantList
+    .map((i) => i.categories)
+    .flat();
+
+  //loop through provided array to count number of times element appears
+  const countOccurences = (array, value) => {
+    return array.reduce((accumulator, element) => {
+      return value === element ? accumulator + 1 : accumulator;
+    }, 0);
+  };
+
+  return (
+    <div className="restaurant-categories">
+      {data.restaurants.categories.sort().map((item) => (
+        <CategoryCard
+          category={item}
+          keyword="places"
+          icon={item}
+          totalNumber={countOccurences(existingCategories, item)}
+          key={item}
+          iconStyle="outside"
+        />
+      ))}
+    </div>
+  );
+};
+
+export default RestaurantCategories;
--- src/pages/EatOut/EatOut.jsx
+++ src/pages/EatOut/EatOut.jsx
@@ -1,23 +1,10 @@
 import React from "react";
 import Breadcrumbs from "../../components/Breadcrumbs/Breadcrumbs";
 import Carousel from "components/Carousel/Carousel";
-import CategoryCard from "../../components/CategoryCard/CategoryCard";
-import data from "../../db.json";
+import RestaurantCategories from "components/RestaurantCategories/RestaurantCategories";
 import "./eat-out.scss";
 
 const EatOut = () => {
-  // loop through all restaurants and push their categories into one array
-  const existingCategories = data.restaurants.restaurantList
-    .map((i) => i.categories)
-    .flat();
-
-  //loop through provided array to count number of times element appears
-  const countOccurences = (array, value) => {
-    return array.reduce((accumulator, element) => {
-      return value === element ? accumulator + 1 : accumulator;
-    }, 0);
-  };
-
   return (
     <div className="eat-out">
       <Breadcrumbs />
@@ -27,18 +14,7 @@ const EatOut = () => {
       </section>
       <div className="eat-out__categories-section">
         <h2 className="eat-out__heading eat-out__heading--small">Categories</h2>
-        <div className="eat-out__restaurant-categories">
-          {data.restaurants.categories.sort().map((item) => (
-            <CategoryCard
-              category={item}
-              keyword="places"
-              icon={item}
-              totalNumber={countOccurences(existingCategories, item)}
-              key={item}
-              iconStyle="outside"
-            />
-          ))}
-        </div>
+        <RestaurantCategories />
       </div>
     </div>
   );

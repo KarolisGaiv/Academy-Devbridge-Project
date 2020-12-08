4adccbc2e6c2435af36c5b00297f4044d9c90c83
Indre Makauskaite <makauskaite.indre@gmail.com>
2020-12-08 19:22:37 +0200

    SOUR-65-Modify EatOut page according to DiscoverRestaurantSection component

--- src/pages/EatOut/EatOut.jsx
+++ src/pages/EatOut/EatOut.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import Breadcrumbs from "../../components/Breadcrumbs/Breadcrumbs";
 import Carousel from "components/Carousel/Carousel";
+import DiscoverRestaurantSection from "components/DiscoverRestaurantSection/DiscoverRestaurantSection";
 import "./eat-out.scss";
 
 const EatOut = () => {
   return (
     <div className="eat-out">
       <Breadcrumbs />
-      <section className="eat-out__discover-restaurants">
+      <section>
         <h2 className="eat-out__heading">Hungry? Find the best place!</h2>
         <Carousel />
+      </section>
+      <section className="eat-out__discover-restaurants">
+        <h3 className="eat-out__small-heading">Discover near you</h3>
+        <DiscoverRestaurantSection />
       </section>
     </div>
   );

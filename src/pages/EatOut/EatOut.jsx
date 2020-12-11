bd01fd009f75ab4f07090561e2882bd3942462c3
Indre Makauskaite <makauskaite.indre@gmail.com>
2020-12-11 20:48:47 +0200

    SOUR-25-Modify EatOut page with NewRestaurantSection

--- src/pages/EatOut/EatOut.jsx
+++ src/pages/EatOut/EatOut.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Breadcrumbs from "../../components/Breadcrumbs/Breadcrumbs";
 import Carousel from "components/Carousel/Carousel";
 import DiscoverRestaurantSection from "components/DiscoverRestaurantSection/DiscoverRestaurantSection";
+import NewRestaurantSection from "components/NewRestaurantSection/NewRestaurantSection";
 import RestaurantCategories from "components/RestaurantCategories/RestaurantCategories";
 import "./eat-out.scss";
 
@@ -20,6 +21,10 @@ const EatOut = () => {
       <section className="eat-out__discover-restaurants">
         <h3 className="eat-out__heading">Discover near you</h3>
         <DiscoverRestaurantSection />
+      </section>
+      <section className="eat-out__new-restaurants">
+        <h3 className="eat-out__heading">New places</h3>
+        <NewRestaurantSection />
       </section>
     </div>
   );

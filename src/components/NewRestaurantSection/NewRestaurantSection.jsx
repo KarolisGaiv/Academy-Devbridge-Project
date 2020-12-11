a3d320d54307f0a377a93fafe6026275c47df880
Indre Makauskaite <makauskaite.indre@gmail.com>
2020-12-11 20:50:04 +0200

    SOUR-25-Create NewRestaurantSection

---
+++ src/components/NewRestaurantSection/NewRestaurantSection.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { RestaurantBigCard } from "../RestaurantCards/RestaurantBigCard/RestaurantBigCard";
+import { Ratings } from "../Rating/maxRatings";
+import "../RestaurantSection/restaurant-section.scss";
+
+class DiscoverRestaurantSection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      restaurantList: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3008/restaurants")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            restaurantList: result.restaurantList,
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        }
+      );
+  }
+
+  render() {
+    const { error, isLoaded, restaurantList } = this.state;
+
+    const restaurants = restaurantList.filter(
+      (restaurant) => restaurant.newPlace === true
+    );
+
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Is loading...</div>;
+    } else {
+      return (
+        <div className="restaurant-section">
+          {restaurants.map((restaurant) => (
+            <RestaurantBigCard
+              key={restaurant.id}
+              checkins={restaurant.checkIns}
+              image={restaurant.image}
+              title={restaurant.name}
+              description={restaurant.description}
+              web={restaurant.website}
+              address={restaurant.address}
+              hours={Ratings.showHours(restaurantList.indexOf(restaurant))}
+              categories={restaurant.categories}
+              rating={Ratings.countRating(restaurantList.indexOf(restaurant))}
+            />
+          ))}
+        </div>
+      );
+    }
+  }
+}
+
+export default DiscoverRestaurantSection;

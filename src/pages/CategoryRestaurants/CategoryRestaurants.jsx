27cc501eca49008b0d6e4278a1bb5d36deb65fe9
Indre Makauskaite <makauskaite.indre@gmail.com>
2020-12-12 19:01:02 +0200

    SOUR-25-Add CategoryRestaurantSection to category page

--- src/pages/CategoryRestaurants/CategoryRestaurants.jsx
+++ src/pages/CategoryRestaurants/CategoryRestaurants.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import Breadcrumbs from "../../components/Breadcrumbs/Breadcrumbs";
+import { CategoryRestaurantSection } from "components/RestaurantSections/CategoryRestaurantSection";
 import "./category-restaurants.scss";
 
 const CategoryRestaurants = () => {
@@ -14,7 +15,7 @@ const CategoryRestaurants = () => {
           The best places for the {category}
         </h1>
         <div className="category-restaurants__list">
-          Insert restaurants here
+          <CategoryRestaurantSection filter="category" category={category} />
         </div>
       </section>
     </div>

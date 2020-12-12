83cfae892f4de661bd1a9f8967452f370631cf86
Indre Makauskaite <makauskaite.indre@gmail.com>
2020-12-12 18:56:16 +0200

    SOUR-25-Modify CategoryRestaurantSection with props

--- src/components/RestaurantSections/CategoryRestaurantSection.jsx
+++ src/components/RestaurantSections/CategoryRestaurantSection.jsx
@@ -1,6 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 import RestaurantSection from "components/RestaurantSections/RestaurantSection/RestaurantSection";
 
-export const CategoryRestaurantSection = () => {
-  return <RestaurantSection filter="category" />;
+export const CategoryRestaurantSection = (props) => {
+  return (
+    <RestaurantSection filter={props.filter} categoryName={props.category} />
+  );
+};
+
+CategoryRestaurantSection.propTypes = {
+  filter: PropTypes.string,
+  category: PropTypes.string,
 };

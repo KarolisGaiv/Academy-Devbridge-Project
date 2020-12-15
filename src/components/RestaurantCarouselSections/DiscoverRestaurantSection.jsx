6b371efab62a99800d510d9f8cbd89ba0e2b7100
Indre Makauskaite <makauskaite.indre@gmail.com>
2020-12-15 15:24:45 +0200

    SOUR-83-Make restaurant carousel component reusable and use it in all restaurant carousel sections

---
+++ src/components/RestaurantCarouselSections/DiscoverRestaurantSection.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import RestaurantCarouselSection from "./RestaurantCarouselSection/RestaurantCarouselSection";
+
+export const DiscoverRestaurantSection = () => {
+  return <RestaurantCarouselSection />;
+};
--- src/components/RestaurantSections/NewRestaurantSection.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import RestaurantSection from "components/RestaurantSections/RestaurantSection/RestaurantSection";
-
-export const NewRestaurantSection = () => {
-  return <RestaurantSection filter="new" />;
-};

b55692163168a38784350eff623e617fd4a6d5a1
Indre Makauskaite <makauskaite.indre@gmail.com>
2020-12-15 17:53:51 +0200

    SOUR-84-Add similar restaurant section to restaurant page

--- src/pages/RestaurantPage/RestaurantPage.jsx
+++ src/pages/RestaurantPage/RestaurantPage.jsx
@@ -1,14 +1,32 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import Breadcrumbs from "components/Breadcrumbs/Breadcrumbs";
+import { SimilarRestaurantSection } from "components/RestaurantCarouselSections/SimilarRestaurantSection";
+import "./restaurant-page.scss";
+import db from "../../db.json";
+
+import { DiscoverRestaurantSection } from "../../components/RestaurantCarouselSections/DiscoverRestaurantSection";
 
 const RestaurantPage = () => {
   const { restaurant } = useParams();
 
+  const restaurantName = restaurant;
+
+  const mainRestaurant = db.restaurants.restaurantList.find(
+    (restaurant) => restaurant.name === restaurantName
+  );
+
   return (
     <div>
       <Breadcrumbs />
-      <section>This will be restaurant {restaurant} page</section>
+      This will be restaurant {restaurant} page
+      <section className="restaurant__similar-restaurants">
+        <h3 className="restaurant__heading">Also you could like</h3>
+        <SimilarRestaurantSection
+          categories={mainRestaurant.categories}
+          filter="similar"
+        />
+      </section>
     </div>
   );
 };

43eb2f9fcdf720ab79b5efee08b1a180616e4e9c
Laurynas Stoma <laurynas.stoma@gmail.com>
2020-12-19 14:22:54 +0200

    Build Reviews Section

--- src/components/Reviews/Review.jsx
+++ src/components/Reviews/Review.jsx
@@ -8,18 +8,20 @@ const Review = (props) => {
   const { username, comment, rating } = props;
 
   return (
-    <CardContainer styleName="card-container--shadow">
-      <div className="review">
-        <div className="review__username">{username}</div>
-        <p className="review__text">{comment}</p>
-        <div className="review__rating">
-          <div className="review_rating-svg">
-            <SVGIcon name="starFilled" />
+    <div className="review">
+      <CardContainer styleName="card-container--shadow">
+        <div className="review__wrapper">
+          <div className="review__username">{username}</div>
+          <p className="review__text">{comment}</p>
+          <div className="review__rating">
+            <div className="review_rating-svg">
+              <SVGIcon name="starFilled" />
+            </div>
+            <span className="review__rating-number">{rating}</span>
           </div>
-          <span className="review__rating-number">{rating}</span>
         </div>
-      </div>
-    </CardContainer>
+      </CardContainer>
+    </div>
   );
 };
 

593c72ecac5f1447eb1c248b706b5eec32d8dcd0
Indre Makauskaite <makauskaite.indre@gmail.com>
2020-12-22 23:22:09 +0200

    SOUR-79-Delete commented code lines from ListSection

--- src/components/ListSection/ListSection.jsx
+++ src/components/ListSection/ListSection.jsx
@@ -2,7 +2,6 @@ import React from "react";
 // import PropTypes from "prop-types"; will be needed when side filter will be merged
 import "./list-section.scss";
 import db from "../../db.json";
-// import { ListItemCard } from "./ListItemCard/ListItemCard";
 import { Pagination } from "../Pagination/Pagination";
 
 export const ListSection = () => {
@@ -16,21 +15,6 @@ export const ListSection = () => {
         {`${productList.length} results for:`}
         <div className="list-section__categories">{` All`}</div>
       </div>
-      {/* {Object.keys(productList).map((key, index) => {
-        return (
-          <ListItemCard
-            key={index}
-            image={productList[key].image}
-            author={productList[key].author}
-            brand={productList[key].brand}
-            title={productList[key].title}
-            name={productList[key].name}
-            bookedUntil={productList[key].bookedUntil}
-            rating={productList[key].rating}
-            quantity={productList[key].quantity}
-          /> */}
-      {/* ); */}
-      {/* })} */}
       <Pagination list={productList} />
     </div>
   );

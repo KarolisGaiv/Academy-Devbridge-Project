d5087d46aa42ab232aa972c828396787010d2239
Indre Makauskaite <makauskaite.indre@gmail.com>
2020-12-29 14:55:17 +0200

    SOUR-106-Link's a tag changed to button tag

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./link.scss";
 
-export const Link = ({ styleName, path, handleClick, children }) => (
-  <a className={`link ${styleName}`} href={path} onClick={handleClick}>
+export const Link = ({ styleName, handleClick, children }) => (
+  <button className={`link ${styleName}`} type="button" onClick={handleClick}>
     {children}
-  </a>
+  </button>
 );
 
 Link.propTypes = {
   styleName: PropTypes.string,
-  path: PropTypes.string,
   handleClick: PropTypes.func,
   children: PropTypes.string,
 };

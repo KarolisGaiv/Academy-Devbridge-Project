74622c4074fe976db7fe81c6fc5ab490076d8580
Aurelija Latvėnaitė <aurelija.latvenaite@gmail.com>
2020-12-31 16:16:18 +0200

    bug/SOUR-87-clickable-pagination-and-button-outline-in-slider add mouse events in scss instead of jsx

--- src/components/SliderNavButtons/SliderNavButtons.jsx
+++ src/components/SliderNavButtons/SliderNavButtons.jsx
@@ -1,47 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import "./slider-nav-buttons.scss";
 import SVGIcon from "components/SVGIcon/SVGIcon";
 
-const SliderNavButtons = ({ leftClicked, rightClicked }) => {
-  const [leftState, setColorLeft] = useState("sliderNavLeft");
-  const [rightState, setColorRight] = useState("sliderNavRight");
-
-  const leftButtonPressed = (color, leftClicked) => {
-    setColorLeft(color);
-    leftClicked();
-  };
-  const rightButtonPressed = (color, rightClicked) => {
-    setColorRight(color);
-    rightClicked();
-  };
+const SliderNavButtons = ({ buttonIcon, leftClicked, rightClicked }) => {
   return (
-    <div className="buttons-wrapper">
-      <button
-        onMouseEnter={() => setColorLeft("sliderNavLeftHover")}
-        onMouseLeave={() => setColorLeft("sliderNavLeft")}
-        onFocus={() => setColorLeft("sliderNavLeftHover")}
-        onBlur={() => setColorLeft("sliderNavLeft")}
-        onClick={() => leftButtonPressed("sliderNavLeftActive", leftClicked)}
-      >
-        <SVGIcon name={leftState} />
+    <>
+      <button className="slider-button" onClick={() => leftClicked()}>
+        <SVGIcon name={buttonIcon} />
       </button>
-      <button
-        onClick={() => rightButtonPressed("sliderNavRightActive", rightClicked)}
-        onMouseEnter={() => setColorRight("sliderNavRightHover")}
-        onMouseLeave={() => setColorRight("sliderNavRight")}
-        onFocus={() => setColorRight("sliderNavRightHover")}
-        onBlur={() => setColorRight("sliderNavRight")}
-      >
-        <SVGIcon name={rightState} />
+      <button className="slider-button" onClick={() => rightClicked()}>
+        <SVGIcon name={buttonIcon} />
       </button>
-    </div>
+    </>
   );
 };
 
 export default SliderNavButtons;
 
 SliderNavButtons.propTypes = {
+  buttonIcon: PropTypes.string,
   leftClicked: PropTypes.func,
   rightClicked: PropTypes.func,
   typeName: PropTypes.string,

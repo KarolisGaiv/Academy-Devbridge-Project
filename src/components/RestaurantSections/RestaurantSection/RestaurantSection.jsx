d79d469d89be9aad9818d2126ff4be1d4c60b307
Aurelija Latvėnaitė <aurelija.latvenaite@gmail.com>
2021-01-13 16:11:01 +0200

    task/SOUR-109 delete not needed change

--- src/components/RestaurantSections/RestaurantSection/RestaurantSection.jsx
+++ src/components/RestaurantSections/RestaurantSection/RestaurantSection.jsx
@@ -10,7 +10,7 @@ class RestaurantSection extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      error: false,
+      error: null,
       isLoaded: false,
       restaurantList: [],
     };

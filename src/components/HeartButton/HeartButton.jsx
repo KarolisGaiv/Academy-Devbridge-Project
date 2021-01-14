f6e6bbdb21b23fc90a49a2e33b77876e96f756c2
Aurelija Latvėnaitė <aurelija.latvenaite@gmail.com>
2021-01-14 22:11:19 +0200

    task/SOUR-151 change HeartButton fill method, add padding where needed

--- src/components/HeartButton/HeartButton.jsx
+++ src/components/HeartButton/HeartButton.jsx
@@ -1,4 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import SVGIcon from "components/SVGIcon/SVGIcon";
 import "./heart-button.scss";
 
-export const HeartButton = () => <input className="heart" type="checkbox" />;
+export const HeartButton = () => {
+  const [reactionCountState, setReactionCountState] = useState({
+    reacted: false,
+  });
+
+  const reactionIconSelect = () => {
+    if (reactionCountState.reacted) {
+      return "heartBtnColored";
+    } else {
+      return "heartBtn";
+    }
+  };
+
+  const onReactionClick = () => {
+    if (reactionCountState.reacted) {
+      setReactionCountState({
+        reacted: false,
+      });
+    } else {
+      setReactionCountState({
+        reacted: true,
+      });
+    }
+  };
+
+  return (
+    <button className="heart-button" onClick={onReactionClick}>
+      <SVGIcon name={reactionIconSelect()} className="heart-button__icon" />
+    </button>
+  );
+};

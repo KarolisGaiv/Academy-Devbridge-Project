947dcc9c6b6bf001f21c68073a33751ceb52248e
Laurynas Stoma <laurynas.stoma@gmail.com>
2021-01-17 17:05:10 +0200

    Newsfeed posts edited states are kept after rerendering within page

--- src/components/NewsFeedCard/UsersReactions/UsersReactions.jsx
+++ src/components/NewsFeedCard/UsersReactions/UsersReactions.jsx
@@ -1,65 +1,43 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import SVGIcon from "../../SVGIcon/SVGIcon";
 
 import "./users-reactions.scss";
 
 const UsersReactions = (props) => {
+  const {
+    type,
+    wishes,
+    likes,
+    reaction,
+    onReactionClick,
+    commentIconHandle,
+    commentCount,
+  } = props;
+
   //Determine which reaction data to take
-  const reactionCountByType = (data) => {
-    if (data.type === "birthday") {
-      return data.wishes;
-    } else if (
-      data.type === "post" ||
-      data.type === "video" ||
-      data.type === "photo"
-    ) {
-      return data.likes;
+  const reactionCountByType = () => {
+    if (type === "birthday") {
+      return wishes;
+    } else if (type === "post" || type === "video" || type === "photo") {
+      return likes;
     }
   };
 
-  const [reactionCountState, setReactionCountState] = useState({
-    reactionCount: reactionCountByType(props.data),
-    reacted: false,
-  });
-
   //Determine wether the post was reacted to and select icon
-  const reactionIconSelect = (data) => {
-    if (reactionCountState.reacted) {
-      if (data.type === "birthday") {
+  const reactionIconSelect = () => {
+    if (reaction) {
+      if (type === "birthday") {
         return "presentBtnColored";
-      } else if (
-        data.type === "post" ||
-        data.type === "video" ||
-        data.type === "photo"
-      ) {
+      } else if (type === "post" || type === "video" || type === "photo") {
         return "heartBtnColored";
       }
     } else {
-      if (data.type === "birthday") {
+      if (type === "birthday") {
         return "presentBtn";
-      } else if (
-        data.type === "post" ||
-        data.type === "video" ||
-        data.type === "photo"
-      ) {
+      } else if (type === "post" || type === "video" || type === "photo") {
         return "heartBtn";
       }
-    }
-  };
-
-  //if reaction was clicked, update reactionCount
-  const onReactionClick = () => {
-    if (reactionCountState.reacted) {
-      setReactionCountState({
-        reactionCount: reactionCountState.reactionCount - 1,
-        reacted: false,
-      });
-    } else {
-      setReactionCountState({
-        reactionCount: reactionCountState.reactionCount + 1,
-        reacted: true,
-      });
     }
   };
 
@@ -68,25 +46,22 @@ const UsersReactions = (props) => {
       <div className="user-reactions__reaction">
         <button className="user-reactions__button" onClick={onReactionClick}>
           <SVGIcon
-            name={reactionIconSelect(props.data)}
+            name={reactionIconSelect()}
             className="user-reactions__reaction-icon svg-icon"
           />
         </button>
         <p className="user-reactions__reaction-count">
-          {reactionCountState.reactionCount}
+          {reactionCountByType()}
         </p>
       </div>
       <div className="user-reactions__comment">
-        <button
-          className="user-reactions__button"
-          onClick={props.commentIconHandle}
-        >
+        <button className="user-reactions__button" onClick={commentIconHandle}>
           <SVGIcon
             name="commentBtn"
             className="user-reactions__comment-icon svg-icon"
           />
         </button>
-        <p className="user-reactions__comment-count">{props.commentCount}</p>
+        <p className="user-reactions__comment-count">{commentCount}</p>
       </div>
     </div>
   );
@@ -95,7 +70,11 @@ const UsersReactions = (props) => {
 export default UsersReactions;
 
 UsersReactions.propTypes = {
-  data: PropTypes.object,
-  commentCount: PropTypes.number,
+  type: PropTypes.string,
+  wishes: PropTypes.number,
+  likes: PropTypes.number,
+  reaction: PropTypes.bool,
+  onReactionClick: PropTypes.func,
   commentIconHandle: PropTypes.func,
+  commentCount: PropTypes.number,
 };

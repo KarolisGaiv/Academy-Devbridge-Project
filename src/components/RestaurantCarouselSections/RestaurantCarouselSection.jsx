2d5630b41ff18acf4258daab55996bdb37d8b1d7
Indre Makauskaite <makauskaite.indre@gmail.com>
2021-01-18 17:36:10 +0200

    SOUR-171-Add primary function to RestaurantCarouselSection

--- src/components/RestaurantCarouselSections/RestaurantCarouselSection.jsx
+++ src/components/RestaurantCarouselSections/RestaurantCarouselSection.jsx
@@ -27,6 +27,59 @@ const RestaurantCarouselSection = (props) => {
 
   const restaurantFilterDate = new Date();
 
+  const getDistanceFromLatLonInKm = (lat1, lon1, lat2, lon2) => {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2 - lat1); // deg2rad below
+    var dLon = deg2rad(lon2 - lon1);
+    var a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(deg2rad(lat1)) *
+        Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c; // Distance in km
+    return Number(d.toFixed(2));
+  };
+
+  const deg2rad = (deg) => {
+    return deg * (Math.PI / 180);
+  };
+
+  let coords = [];
+
+  const dynamicSort = (property) => {
+    let sortOrder = 1;
+    if (property[0] === "-") {
+      sortOrder = -1;
+      property = property.substr(1);
+    }
+    return function (a, b) {
+      let result =
+        a[property] < b[property] ? -1 : a[property] > b[property] ? 1 : 0;
+      return result * sortOrder;
+    };
+  };
+
+  // const getRestaurantListWithDistance = () => {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(function (position) {
+  //       coords.push(position.coords.latitude);
+  //       coords.push(position.coords.longitude);
+  //       let restaurantListWithDistance = data.map((restaurant) => ({
+  //         ...restaurant,
+  //         distance: getDistanceFromLatLonInKm(
+  //           position.coords.latitude,
+  //           position.coords.longitude,
+  //           restaurant.latitude,
+  //           restaurant.longitude
+  //         ),
+  //       }));
+  //       console.log(restaurantListWithDistance.sort(dynamicSort("distance")));
+  //     });
+  //   }
+  // };
+
   let restaurants = data;
 
   switch (props.filter) {
@@ -36,6 +89,25 @@ const RestaurantCarouselSection = (props) => {
         (restaurant) =>
           Date.parse(restaurant.openingDate) > restaurantFilterDate
       );
+      break;
+    case "discover":
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+          coords.push(position.coords.latitude);
+          coords.push(position.coords.longitude);
+          restaurants = data
+            .map((restaurant) => ({
+              ...restaurant,
+              distance: getDistanceFromLatLonInKm(
+                position.coords.latitude,
+                position.coords.longitude,
+                restaurant.latitude,
+                restaurant.longitude
+              ),
+            }))
+            .sort(dynamicSort("distance"));
+        });
+      }
       break;
     default:
       restaurants = data;
